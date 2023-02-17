deb91e6f968fb65717ee7fcc3fd3953e71c64556
Jose De Freitas <josedefreitas91@gmail.com>
2023-02-17 17:15:37 -0500

    add postgres uri

--- alembic/env.py
+++ alembic/env.py
@@ -48,7 +48,7 @@ def run_migrations_offline() -> None:
 
     """
     # url = config.get_main_option("sqlalchemy.url")
-    url = get_url()
+    url = os.getenv("POSTGRES_URI", get_url())
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -76,7 +76,7 @@ def run_migrations_online() -> None:
 
     configuration = config.get_section(config.config_ini_section)
     if configuration:
-        configuration["sqlalchemy.url"] = get_url()
+        configuration["sqlalchemy.url"] = os.getenv("POSTGRES_URI", get_url())
     connectable = engine_from_config(configuration, prefix="sqlalchemy.", poolclass=pool.NullPool) # type: ignore
 
     with connectable.connect() as connection:
